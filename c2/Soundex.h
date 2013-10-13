646d86b2c67dd9d980aed75d8e2af3e3dfee800c
tsu-nera <fox10225fox@gmail.com>
2013-10-13 19:49:37 +0900

    2.7

--- c2/Soundex.h
+++ c2/Soundex.h
@@ -10,15 +10,32 @@
 
 #include <string>
 
+static const size_t MaxCodeLength{4};
+
 class Soundex{
 public:
   std::string encode(const std::string& word) const {
-    return zeroPad(word);
+    return zeroPad(head(word) + encodedDigits(word));
   }
 
 private:
+
+  std::string head(const std::string& word) const {
+    return word.substr(0, 1);
+  }
+
+  std::string encodedDigits(const std::string& word) const {
+    if (word.length() > 1) return encodeDigit();
+    return "";
+  }
+
   std::string zeroPad(const std::string& word) const {
-    return word + "000";
+    auto zerosNeeded = MaxCodeLength - word.length();
+    return word + std::string(zerosNeeded, '0');
+  }
+
+  std::string encodeDigit() const {
+    return "1";
   }
 };
 
--- c2/SoundexTest.cpp
+++ c2/SoundexTest.cpp
@@ -24,3 +24,10 @@ TEST_F(SoundexEncoding, PadsWithZerosToEnsureThreeDigits)
 {
   ASSERT_THAT(soundex.encode("I"), Eq("I000"));
 }
+
+TEST_F(SoundexEncoding, ReplacesConsonantsWithAppropriateDigits)
+{
+  EXPECT_THAT(soundex.encode("Ab"), Eq("A100"));
+  EXPECT_THAT(soundex.encode("Ac"), Eq("A200"));
+}
+
