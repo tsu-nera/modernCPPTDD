6a2b959a8f9a08003d88dcabb116061c7c13c393
tsu-nera <fox10225fox@gmail.com>
2013-10-15 03:45:49 +0900

    1

--- rakefile.rb
+++ rakefile.rb
@@ -25,7 +25,7 @@ task :run => TARGET do
   sh "./#{TARGET}"
 end
 
-SRCS = FileList["main.cpp","c2/*.cpp", "c3/*.cpp", "c5/*.cpp", "c6/*.cpp"]
+SRCS = FileList["main.cpp","c2/*.cpp", "c3/*.cpp", "c5/*.cpp", "c6/*.cpp", "c7/*.cpp"]
 OBJS = SRCS.ext('o')
 
 file TARGET => OBJS do |t|
