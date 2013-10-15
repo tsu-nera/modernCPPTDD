3e5b5abeb82346d35b16ddea78c2b58167d99939
tsu-nera <fox10225fox@gmail.com>
2013-10-15 20:25:30 +0900

    288

--- tpp/Soundex.h
+++ tpp/Soundex.h
@@ -12,7 +12,27 @@
 class Soundex {
 public:
    std::string encode(const std::string& word) const {
-      return "A000";
+      std::string code("");
+      code += head(word) + encodeTail(word);
+      return zeroPad(code);
+   }
+
+   char head(const std::string& word) const {
+      return word[0];
+   }
+
+   std::string encodeTail(const std::string& word) const {
+      if (word[1] == 0) return "";
+      return "1";
+   }
+
+   std::string zeroPad(const std::string& code) const {
+      return code + (hasEncodedCharacters(code) ? "00" : "000");
+   }
+
+   bool hasEncodedCharacters(const std::string& code) const {
+      return code[1] != 0;
    }
 };
+
 #endif
--- tpp/SoundexTest.cpp
+++ tpp/SoundexTest.cpp
@@ -2,12 +2,16 @@
 
 #include "Soundex.h"
 
-TEST_GROUP(SoundexEncoding) {};
+TEST_GROUP(SoundexEncoding) {
+  Soundex soundex;
+};
 
 TEST(SoundexEncoding, AppendsZerosToWordForOneLetterWord) {
-   Soundex soundex;
+  CHECK_EQUAL("A000", soundex.encode("A"));
+  CHECK_EQUAL("B000", soundex.encode("B"));
+}
 
-   auto encoded = soundex.encode("A");
-
-   CHECK_EQUAL("A000", encoded);
+TEST(SoundexEncoding, ReplaceConsonantsWithAppropriateDigits)
+{
+  CHECK_EQUAL("A100", soundex.encode("Ab"));
 }
